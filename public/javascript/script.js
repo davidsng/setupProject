9fcecb55774d1f5c876b77efe553665fb21d1c69
davidsng <davidsnghs@gmail.com>
2018-02-15 10:07:48 +0800

    Teaching Justin

---
+++ public/javascript/script.js
@@ -0,0 +1,7 @@
+function add_number () {
+    var first_number = parseInt(document.getElementById("Text1").value);
+    var second_number = parseInt(document.getElementById("Text2").value);
+    var result = first_number + second_number;
+    document.getElementById("txtresult").value = result;
+    console.log(result)
+}
